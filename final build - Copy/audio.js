3e2e1a22d56538c1d5805d0b5c623102cbb4e9e6
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-01-19 22:35:01 +0530

    final without increament

---
+++ final build - Copy/audio.js
@@ -0,0 +1,19 @@
+const audio = {
+    BackgroundAudio: new Howl ({
+        src:'./audio/BackgroundAudio.mp3',
+        loop:true,
+        html5: true
+    }),
+    collisionAudio: new Howl ({
+        src:'./audio/collision.mp3',
+        html5: true
+    }),
+    ProjectileAudio: new Howl ({
+        src:'./audio/ProjectileAudio.mp3',
+        html5: true
+    }),
+    GameEndAudio: new Howl ({
+        src:'./audio/GameEndAudio.mp3',
+        html5: true
+    }) 
+}
